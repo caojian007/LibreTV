392f2c9a1084e9810769e6972bd2abda61c0ce54
caojian007 <41392297@qq.com>
2025-08-25 10:14:22 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 // 全局常量配置
 const PROXY_URL = '/proxy/';    // 适用于 Cloudflare, Netlify (带重写), Vercel (带重写)
-// const HOPLAYER_URL = 'https://hoplayer.com/index.html';// const HOPLAYER_URL = 'https://hoplayer.com/index.html'；
-const const   常量 SEARCH_HISTORY_KEY = 'videoSearchHistory'；SEARCH_HISTORY_KEY = 'videoSearchHistory';
-const const   常量 MAX_HISTORY_ITEMS = 5；MAX_HISTORY_ITEMS = 5;
+// const HOPLAYER_URL = 'https://hoplayer.com/index.html';
+const SEARCH_HISTORY_KEY = 'videoSearchHistory';
+const MAX_HISTORY_ITEMS = 5;
 
 // 密码保护配置
 // 注意：PASSWORD 环境变量是必需的，所有部署都必须设置密码以确保安全
@@ -13,11 +13,11 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-       名字:而“LibreTVname: '小曹影视',
-    url:“https://https://bt.2014818.xyz/”,url: 'https://bt.2014818.xyz/',
+    name: 'LibreTV',
+    url: 'https://libretv.is-an.org',
     description: '免费在线视频搜索与观看平台',
-       标识:“图像/ logo.png”,logo: 'image/logo.png',
-       版本:“1.0.3”version: '1.0.3'
+    logo: 'image/logo.png',
+    version: '1.0.3'
 };
 
 // API站点配置
